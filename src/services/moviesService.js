38fa51b15081a90bd4bf4cbca253fafe186a6b79
luc <luctunechi45@gmail.com>
2018-11-05 19:19:49 +0200

    save movie

--- src/services/moviesService.js
+++ src/services/moviesService.js
@@ -11,7 +11,14 @@ export function getMovie(movieId) {
   return http.get(apiEndpoint + "/" + movieId)
 }
 
-export function saveMovie(movie) {}
+export function saveMovie(movie) {
+  if(movie._id){
+    const body = {...movie}
+    delete body._id
+    return http.put(apiEndpoint + '/' + movie._id, body)
+  }
+  return http.post(apiEndpoint, movie)
+}
 
 export function deleteMovie(movieId) {
   http.delete(apiEndpoint + "/" + movieId)
