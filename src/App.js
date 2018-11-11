04db28212e6f0b5daf7e76864d792b1a03da7f15
luc <luctunechi45@gmail.com>
2018-11-12 00:55:34 +0200

    logout component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,45 @@
+// object from third party libraries
 import React, { Component } from "react"
 import { Route, Switch, Redirect } from "react-router-dom"
-import MovieForm from "./components/movieForm"
 import { ToastContainer } from "react-toastify"
+import jwtDecode from "jwt-decode"
+
+// import components from application
+import MovieForm from "./components/movieForm"
 import NavBar from "./components/Navbar"
 import Movies from "./components/movies"
 import Customers from "./components/customers"
 import Rentals from "./components/rentals"
 import NotFound from "./components/notFound"
 import LoginForm from "./components/loginForm"
+import Logout from "./components/logout"
 import RegisterForm from "./components/registerForm"
+
+// css properties
 import "react-toastify/dist/ReactToastify.css"
 import "./App.css"
 
 class App extends Component {
+  state = {}
+
+  componentDidMount() {
+    try {
+      const jwt = localStorage.getItem("token")
+      const user = jwtDecode(jwt)
+      this.setState({ user })
+    } catch (ex) {}
+  }
+
   render() {
     return (
       <React.Fragment>
         <ToastContainer />
-        <NavBar />
+        <NavBar user={this.state.user} />
         <main className="container">
           <Switch>
             <Route path="/login" component={LoginForm} />
             <Route path="/register" component={RegisterForm} />
+            <Route path="/logout" component={Logout} />
             <Route path="/movies/:id" component={MovieForm} />
             <Route path="/movies" component={Movies} />
             <Route path="/customers" component={Customers} />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react"
 
 import { Link, NavLink } from "react-router-dom"
 
-const NavBar = props => {
+const NavBar = ({ user }) => {
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
@@ -22,31 +22,39 @@ const NavBar = props => {
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/movies">
-                Movies
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/customers">
-                Customers
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/rentals">
-                Rentals
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/login">
-                Login
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/register">
-                Register
-              </NavLink>
-            </li>
+            <NavLink className="nav-link" to="/movies">
+              Movies
+            </NavLink>
+
+            <NavLink className="nav-link" to="/customers">
+              Customers
+            </NavLink>
+
+            <NavLink className="nav-link" to="/rentals">
+              Rentals
+            </NavLink>
+            {!user && (
+              <React.Fragment>
+                <NavLink className="nav-link" to="/login">
+                  Login
+                </NavLink>
+
+                <NavLink className="nav-link" to="/register">
+                  Register
+                </NavLink>
+              </React.Fragment>
+            )}
+            {user && (
+              <React.Fragment>
+                <NavLink className="nav-link" to="/profile">
+                  {user.name}
+                </NavLink>
+
+                <NavLink className="nav-link" to="/logout">
+                  Logout
+                </NavLink>
+              </React.Fragment>
+            )}
           </ul>
         </div>
       </nav>
